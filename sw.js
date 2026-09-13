5d5d0cbe1ccfdf56705a51c7f8c0c94869fc62e3
chrispapasotiris04-prog <chrispapasotiris04@gmail.com>
2026-09-13 12:38:52 +0300

    upd

--- sw.js
+++ sw.js
@@ -15,4 +15,4 @@ var isNav=e.request.mode==='navigate';var isIndex=isNav&&(url.pathname==='/'||/\
 if(isIndex){e.respondWith(readForce().then(function(f){
 if(f==='1'){return caches.open(LASTGOOD).then(function(c){return c.match('index.html').then(function(r){if(r)return r;return fetch(e.request);});});}
 return fetch(e.request).then(function(res){if(res.ok){var cp=res.clone();caches.open(PENDING).then(function(c){c.put('index.html',cp);});}return res;}).catch(function(){return caches.match(e.request).then(function(r){if(r)return r;return caches.open(LASTGOOD).then(function(c){return c.match('index.html');});}).then(function(r){return r||new Response('Εκτός δικτύου και χωρίς αποθηκευμένη έκδοση.',{status:503,headers:{'Content-Type':'text/plain; charset=utf-8'}});});});}));return;}
-e.respondWith(fetch(e.request).then(function(res){if(res.ok||res.type==='opaque'){var cp=res.clone
+e.respondWith(fetch(e.request).then(function(res){if(res.ok||res.type==='opaque'){var cp=res.clone();caches.open(CACHE).then(function(c){c.put(e.request,cp);});}return res;}).catch(function(){return caches.match(e.request).then(function(r){return r||Response.error();});}));});
